ca06ec9f4682d8cd51278bdc854c5d0d72e77f82
Adrian Rangel <adrian.rangel@gmail.com>
2014-02-22 15:10:18 -0600

    use ruby 2.1 and postgress

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def title(value)
     unless value.nil?
-      @title = "#{value} | CuantoDebemos"      
+      @title = "#{value} | CuantoDebemos"
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 CuantoDebemos::Application.routes.draw do
-      
+
   root "pages#home"
-  
+
   get "/home", to: "pages#home", as: "home"
 end
