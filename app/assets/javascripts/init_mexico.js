5cf6983ebcc8a07108936aea7fd98991f9dcdbde
Montse Lozano Dieck <montse_967@hotmail.com>
2014-02-23 12:11:54 -0600

    links y la vista

--- app/assets/javascripts/init_mexico.js
+++ app/assets/javascripts/init_mexico.js
@@ -1,9 +1,19 @@
 jQuery(function($){
     jQuery('#vmap').vectorMap({
             map: 'mx_es',
-            enableZoom: true,
+            enableZoom: false,
             showTooltip: true,
-            selectedRegion: 'NLE'
+            selectedRegion: 'NLE' ,
+            backgroundColor: 'transparent',
+            color: '#EAE9F0',
+            hoverColor: '#BF005D',
+            selectedColor: '#BF005D',
+            scaleColors: ['#b6d6ff', '#005ace'],
+            onRegionClick: function (event, code, region, url) 
+            {
+                window.location.replace(url); 
+            }
         });
+
 
 });
