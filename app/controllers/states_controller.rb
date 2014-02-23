b966e52f883be4ba004d7622b8e9ae35ff28568a
luis loya <jl.loya@gmail.com>
2014-02-23 10:39:41 -0600

    Added new instansce variables to states controller

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -2,5 +2,8 @@ class StatesController < ApplicationController
   def show
     @state = State.find_by :state=>params[:id]
     @debt = @state.financials["debt"].to_i * 1000000
+    @population = @state.financials["people"]
+    
+    @prices = Indicator.all.to_a.each_with_object({}){ |c,h| h[c.name] = c.value }
   end
 end
