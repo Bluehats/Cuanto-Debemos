f33be4a472a3c08ecb4c6ded82ba9750b244ffc6
Adrian Rangel <adrian.rangel@gmail.com>
2014-02-24 14:27:55 -0600

    last commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController
 
   def home
-    @region = JSON.parse(RestClient.get("http://freegeoip.net/json/"))['region_code'] || 19
+    @region = JSON.parse(RestClient.get("http://freegeoip.net/json/#{request.ip}"))['region_code']
+
+    unless @region.to_i.between?(1,32)
+      @region = 19
+    end
+
     redirect_to state_path(@region)
   end
 
