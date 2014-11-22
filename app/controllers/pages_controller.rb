9f329fb674ea80ec1082dcdf8622523dbf33e0ab
Adrian Rangel <adrian.rangel@gmail.com>
2014-11-22 08:55:03 -0600

    freeip doesn't return region code anymore

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
-    @region = JSON.parse(RestClient.get("http://freegeoip.net/json/#{request['HTTP_X_FORWARDED_FOR']}"))['region_code']
-    @region = 19 unless @region.to_i.between?(1,32)
+    # @region = JSON.parse(RestClient.get("http://freegeoip.net/json/#{request['HTTP_X_FORWARDED_FOR']}"))['region_code']
+    @region = 19 # unless @region.to_i.between?(1,32)
 
     redirect_to state_path(@region)
   end
