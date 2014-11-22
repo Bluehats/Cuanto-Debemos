3bc81215a6844bc9ae8fc08f5e29b1f0427514db
Adrian Rangel <adrian.rangel@gmail.com>
2014-11-22 08:41:37 -0600

    rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 CuantoDebemos::Application.routes.draw do
 
-  get "states/show"
-  root "pages#home"
+  get 'states/show'
+  root 'pages#home'
 
-  get "/estado/:id", :to=> "states#show", :as=>"state"
+  get '/estado/:id', to: 'states#show', as: 'state'
 
-  get "/home", to: "pages#home", as: "home"
+  get '/home', to: 'pages#home', as: 'home'
 end
